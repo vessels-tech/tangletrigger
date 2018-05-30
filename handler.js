7e9c067a9af35266b7ff8071b9812552bf58b57a
Lewis Daly <lewisdaly@me.com>
2018-05-30 09:02:55 +0800

    Add readme

--- handler.js
+++ handler.js
@@ -6,17 +6,4 @@ const iota = new IOTA(process.env.IOTA_URL);
 module.exports.checkAddress = (event, context, callback) => {
 
   console.log("CHECKING ADDRESS:", process.env.IOTA_ADDRESS);
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
---
+++ readme.md
@@ -0,0 +1,6 @@
+# TangleTrigger
+
+TangleTrigger is a lambda trigger which sends SNS notifications when payments are observed in the tangle.
+
+
+Read the blog post [here](insert link).
